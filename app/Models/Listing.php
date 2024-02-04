e3a18ceefd3d5cb1821f1e59654a96d75a055538
Miha Stele <stele1000@gmail.com>
2024-02-04 13:56:56 +0100

    Update

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,23 +2,10 @@
 
 namespace App\Models;
 
-class Listing
-{
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Help me find a home for my dog',
-                'description' => 'I have a dog that needs a new home. He loves to play and is good with kids. Can you help?',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Need a sitter for my cat',
-                'description' => 'I will be out of town for a few days and need someone to feed my cat.',
-            ]
-        ];
-    }
-}
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-?>
+class Listing extends Model
+{
+    use HasFactory;
+}
